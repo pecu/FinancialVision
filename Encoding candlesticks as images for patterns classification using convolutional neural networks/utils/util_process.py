[bug] fix the calculation of the trend

Encoding candlesticks as images for patterns classification using convolutional neural networks/utils/util_process.py
@@ -101,7 +101,9 @@ def get_trend(slope):
     '''
     slope = np.array(slope)
     thres = 7.214285714286977e-05
-    if (slope >= thres) or (slope <= -thres):
+    if (slope >= thres):
         return 1
+    elif (slope <= -thres)
+        return -1
     else:
         return 0