dea5f90f3ac4e428551894e391f8947ae10c9005
Bigmoumou <o1r2g3a4n5i6z7e8@gmail.com>
2020-07-22 13:43:42 +0800

    [feat] add gaf-encoding code to utils of encoding candlestick as image

--- Encoding candlesticks as images for patterns classification using convolutional neural networks/utils/util_process.py
+++ Encoding candlesticks as images for patterns classification using convolutional neural networks/utils/util_process.py
@@ -25,6 +25,54 @@ def load_pkl(pkl_name):
     return data
 
 
+def ts2gasf(ts, max_v, min_v):
+    '''
+    Args:
+        ts (numpy): (N, )
+        max_v (int): max value for normalization
+        min_v (int): min value for normalization
+
+    Returns:
+        gaf_m (numpy): (N, N)
+    '''
+    # Normalization : 0 ~ 1
+    if max_v == min_v:
+        gaf_m = np.zeros((len(ts), len(ts)))
+    else:
+        ts_nor = np.array((ts-min_v) / (max_v-min_v))
+        # Arccos
+        ts_nor_arc = np.arccos(ts_nor)
+        # GAF
+        gaf_m = np.zeros((len(ts_nor), len(ts_nor)))
+        for r in range(len(ts_nor)):
+            for c in range(len(ts_nor)):
+                gaf_m[r, c] = np.cos(ts_nor_arc[r] + ts_nor_arc[c])
+    return gaf_m
+
+
+def get_gasf(arr):
+    '''Convert time-series to gasf    
+    Args:
+        arr (numpy): (N, ts_n, 4)
+
+    Returns:
+        gasf (numpy): (N, ts_n, ts_n, 4)
+
+    Todos:
+        add normalization together version
+    '''
+    arr = arr.copy()
+    gasf = np.zeros((arr.shape[0], arr.shape[1], arr.shape[1], arr.shape[2]))
+    for i in range(arr.shape[0]):
+        for c in range(arr.shape[2]):
+            each_channel = arr[i, :, c]
+            c_max = np.amax(each_channel)
+            c_min = np.amin(each_channel)
+            each_gasf = ts2gasf(each_channel, max_v=c_max, min_v=c_min)
+            gasf[i, :, :, c] = each_gasf
+    return gasf
+
+
 def gasf2ts(arr):
     '''
     Args:
